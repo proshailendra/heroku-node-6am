596f5b200487c1b7ab5f2cb2a4618542f7d68edd
Shailendra Chauhan <pro.shailendra@gmail.com>
2016-10-19 06:18:30 +0530

    updated code

--- server.js
+++ server.js
@@ -1,37 +1,10 @@
-var express = require("express"),
-    bodyParser = require("body-parser"),
-    expressSession = require("express-session"),
-    path = require("path"),
-    apiRoutes = require("./server/routes/api.routes"),
-    webRoutes = require("./server/routes/web.routes");
-
-var app = express();
-
-//session
-app.use(expressSession({
-    secret: 'mytoken',
-    saveUninitialized: true,
-    resave: true
-}));
-
-//application/json
-app.use(bodyParser.json());
-//www-form-data
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//angular app
-app.use(express.static("./app"));
-//packages
-app.use(express.static("./node_modules"));
-
-//routes
-app.use("/", webRoutes); //angular app
-app.use("/api", apiRoutes); //express api
-//angular html5mode fixing
-app.use("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/index.html"));
-});
-
+var express = require('express')
+var app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
 var port = process.env.port || 3000;
 app.listen(port, function() {
     console.log("Server is running at http://localhost:" + port);
