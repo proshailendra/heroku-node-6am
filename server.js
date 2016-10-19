93b86923c789e38570a2d1576d3aa356753c15bd
Shailendra Chauhan <pro.shailendra@gmail.com>
2016-10-19 06:26:53 +0530

    updaed port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
  
 app.get('/', function (req, res) {
   res.send('Hello World')
-})
+});
  
-var port = process.env.port || 3000;
+var port = process.env.port || 1300;
 app.listen(port, function() {
     console.log("Server is running at http://localhost:" + port);
 });
